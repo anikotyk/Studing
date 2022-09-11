287b96952c5ad5df155262e8077469a89e99d17c
Onna <anikotyk@gmail.com>
2022-09-11 23:24:35 +0300

    Update task3.rb

--- Grade3(2022-2023)/Ruby/Lab2/task3.rb
+++ Grade3(2022-2023)/Ruby/Lab2/task3.rb
@@ -1,10 +1,10 @@
 num = 10000100101
 
 res = 0
-deg = 0
-num.to_s.split('').reverse.each{ |char|
+deg = num.to_s.length - 1
+num.to_s.split('').each{ |char|
   res += char.to_i * 2**deg
-  deg += 1
+  deg -= 1
 }
 
 puts "Answer: #{res}"
