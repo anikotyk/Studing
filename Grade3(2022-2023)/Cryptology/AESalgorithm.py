1393fca1adc1de9203ea99005aa975996e011082
Onna <anikotyk@gmail.com>
2023-06-03 18:03:06 +0200

    Added cryptology exam

--- Grade3(2022-2023)/Cryptology/AESalgorithm.py
+++ Grade3(2022-2023)/Cryptology/AESalgorithm.py
@@ -1,308 +1,195 @@
-def xor(left, right):
-    final = ""
-    l_len = len(left)
-    for i in range(0, l_len):
-        if left[i] == right[i]:
-            final = final + '0'
-        elif left[i] != right[i]:
-            final = final + '1'
-    return final
-
-
-def col_to_row(val):
-    row = []
-    for i in range(0, 4):
-        col = []
-        for j in range(0, 4):
-            col = col + [val[j][i]]
-        row = row + [col]
-    return row
-
-
-def row_to_col(val):
-    col = []
-    for i in range(0, 4):
-        row = []
-        for j in range(0, 4):
-            row = row + [val[j][i]]
-        col = col + [row]
-    return col
-
-
-def substitute_byte(state):
-    row = []
-    for i in range(0, 4):
-        col = []
-        for j in range(0, 4):
-            col = col + [substitution_box(state[i][j])]
-        row = row + [col]
-    return row
-
-
-def shift_row(sub_bytes):
-    state_array = []
-
-    first = sub_bytes[0]
-    state_array = state_array + [first]
-
-
-    second = [sub_bytes[1][1]] + [sub_bytes[1][2]] + [sub_bytes[1][3]] + [sub_bytes[1][0]]
-    state_array = state_array + [second]
-
-    third = [sub_bytes[2][2]] + [sub_bytes[2][3]] + [sub_bytes[2][0]] + [sub_bytes[2][1]]
-    state_array = state_array + [third]
-
-    four = [sub_bytes[3][3]] + [sub_bytes[3][0]] + [sub_bytes[3][1]] + [sub_bytes[3][2]]
-    state_array = state_array + [four]
-
-    return state_array
-
-def h2b(val):
-    hex_val = {"0": "0000", "1": "0001", "2": "0010", "3": "0011",
-               "4": "0100", "5": "0101", "6": "0110", "7": "0111",
-               "8": "1000", "9": "1001", "a": "1010", "b": "1011",
-               "c": "1100", "d": "1101", "e": "1110", "f": "1111"}
-    m_len = len(val)
-    final = ""
-    for i in range(0, m_len):
-        final = final + hex_val[val[i]]
-    return final
-
-
-def b2h(val):
-    bin_val = {"0000": "0", "0001": "1", "0010": "2", "0011": "3",
-               "0100": "4", "0101": "5", "0110": "6", "0111": "7",
-               "1000": "8", "1001": "9", "1010": "a", "1011": "b",
-               "1100": "c", "1101": "d", "1110": "e", "1111": "f"}
-    m_len = len(val)
-    lst = []
-    final = ""
-    count = 0
-    i = 0
-    while count != m_len:
-        lst = lst + [val[count:count + 4]]
-        final = final + bin_val[lst[i]]
-        i = i + 1
-        count = count + 4
-
-    return final
-
-
-def substitution_box(rc):
-    hex_val = {"0": "0", "1": "1", "2": "2", "3": "3",
-               "4": "4", "5": "5", "6": "6", "7": "7",
-               "8": "8", "9": "9", "a": "10", "b": "11",
-               "c": "12", "d": "13", "e": "14", "f": "15"}
-
-    rijndael_s_box = [["63", "7c", "77", "7b", "f2", "6b", "6f", "c5", "30", "01", "67", "2b", "fe", "d7", "ab", "76"],
-                      ["ca", "82", "c9", "7d", "fa", "59", "47", "f0", "ad", "d4", "a2", "af", "9c", "a4", "72", "c0"],
-                      ["b7", "fd", "93", "26", "36", "3f", "f7", "cc", "34", "a5", "e5", "f1", "71", "d8", "31", "15"],
-                      ["04", "c7", "23", "c3", "18", "96", "05", "9a", "07", "12", "80", "e2", "eb", "27", "b2", "75"],
-                      ["09", "83", "2c", "1a", "1b", "6e", "5a", "a0", "52", "3b", "d6", "b3", "29", "e3", "2f", "84"],
-                      ["53", "d1", "00", "ed", "20", "fc", "b1", "5b", "6a", "cb", "be", "39", "4a", "4c", "58", "cf"],
-                      ["d0", "ef", "aa", "fb", "43", "4d", "33", "85", "45", "f9", "02", "7f", "50", "3c", "9f", "a8"],
-                      ["51", "a3", "40", "8f", "92", "9d", "38", "f5", "bc", "b6", "da", "21", "10", "ff", "f3", "d2"],
-                      ["cd", "0c", "13", "ec", "5f", "97", "44", "17", "c4", "a7", "7e", "3d", "64", "5d", "19", "73"],
-                      ["60", "81", "4f", "dc", "22", "2a", "90", "88", "46", "ee", "b8", "14", "de", "5e", "0b", "db"],
-                      ["e0", "32", "3a", "0a", "49", "06", "24", "5c", "c2", "d3", "ac", "62", "91", "95", "e4", "79"],
-                      ["e7", "c8", "37", "6d", "8d", "d5", "4e", "a9", "6c", "56", "f4", "ea", "65", "7a", "ae", "08"],
-                      ["ba", "78", "25", "2e", "1c", "a6", "b4", "c6", "e8", "dd", "74", "1f", "4b", "bd", "8b", "8a"],
-                      ["70", "3e", "b5", "66", "48", "03", "f6", "0e", "61", "35", "57", "b9", "86", "c1", "1d", "9e"],
-                      ["e1", "f8", "98", "11", "69", "d9", "8e", "94", "9b", "1e", "87", "e9", "ce", "55", "28", "df"],
-                      ["8c", "a1", "89", "0d", "bf", "e6", "42", "68", "41", "99", "2d", "0f", "b0", "54", "bb", "16"]]
-
-    substitution = rijndael_s_box[int(hex_val[rc[0]])][int(hex_val[rc[1]])]
-    return substitution
-
-
-def multiply_by_02(inp):
-    bin_val = h2b(inp[0]) + h2b(inp[1])
-    final = ""
-    if bin_val[0] == '1':
-        bin_val = bin_val[1:len(bin_val)] + '0'
-        final = xor(bin_val, h2b('1b'))
-    elif bin_val[0] == '0':
-        final = bin_val[1:len(bin_val)] + '0'
-
-    return final
-
-def multiply_by_03(inp):
-    mix = multiply_by_02(inp)
-    final = xor(mix, h2b(inp))
-
-    return final
-
-def row0(row):
-    xor01 = xor(multiply_by_02(row[0]), multiply_by_03(row[1]))
-    xor23 = xor(h2b(row[2]), h2b(row[3]))
-    final_xor = xor(xor01, xor23)
-
-    return b2h(final_xor)
-
-
-def row1(row):
-    xor01 = xor(h2b(row[0]), multiply_by_02(row[1]))
-    xor23 = xor(multiply_by_03(row[2]), h2b(row[3]))
-    final_xor = xor(xor01, xor23)
-
-    return b2h(final_xor)
-
-
-def row2(row):
-    xor01 = xor(h2b(row[0]), h2b(row[1]))
-    xor23 = xor(multiply_by_02(row[2]), multiply_by_03(row[3]))
-    final_xor = xor(xor01, xor23)
-
-    return b2h(final_xor)
-
-
-def row3(row):
-    xor01 = xor(multiply_by_03(row[0]), h2b(row[1]))
-    xor23 = xor(multiply_by_02(row[3]), h2b(row[2]))
-    final_xor = xor(xor01, xor23)
-
-    return b2h(final_xor)
-
-
-def mix_col(s_row):
-    final_row = []
-    for i in range(0, 4):
-        tmp_row = [row0(s_row[i])] + [row1(s_row[i])] + [row2(s_row[i])] + [row3(s_row[i])]
-        final_row = final_row + [tmp_row]
-
-    return final_row
-
-
-def key_expansion(s_key, rnd):
-    keys = []
-    for pos in range(0, 4):
-        if pos == 0:
-            first_col = []
-            tmp = s_key[3]
-            t_len = len(tmp)
-
-            for i in range(1, t_len):
-                first_col = first_col + [tmp[i]]
-            first_col = first_col + [tmp[0]]
-            col = []
-
-            f_len = len(first_col)
-            for i in range(0, f_len):
-                col = col + [substitution_box(first_col[i])]
-
-            tmp_key = []
-            for i in range(0, 4):
-                sub_key = xor(h2b(s_key[0][i]), h2b(rnd[i]))
-                sub_key1 = xor(sub_key, h2b(col[i]))
-                tmp_key = tmp_key + [str(b2h(sub_key1))]
-
-            keys = keys + [tmp_key]
-
-        elif pos > 0:
-            first_col = []
-
-            for i in range(0, 4):
-                sub_key = xor(h2b(s_key[pos][i]), h2b(keys[pos - 1][i]))
-                first_col = first_col + [str(b2h(sub_key))]
-
-            keys = keys + [first_col]
-    return keys
-
-
-def add_round(plain_text, keys):
-    row = []
-    for i in range(0, 4):
-        col = []
-        for j in range(0, 4):
-            tmp = xor(h2b(keys[i][j]), h2b(plain_text[i][j]))
-            col = col + [b2h(tmp)]
-        row = row + [col]
-
-    return col_to_row(row)
-
-
-def valid_block_size(msg):
-    m_len = len(msg)
-    final = msg
-    if m_len > 32:
-        print("Not a valid size block, Exceeding Block size!")
-        final = final[0:32]
-        print("After filler:", final)
-        return final
-    elif m_len % 32 != 0:
-        print("Not a valid size block")
-        for i in range(abs(32 - (m_len % 32))):
-            final = final + "0"
-        print("After filler:", final)
-        return final
-    else:
-        print("Valid size block")
-    return msg
-
-
-def key_and_text_to_matrix(key_string):
-    arr = [["00" for _ in range(4)] for _ in range(4)]
-    row = 0
-    col = 0
-    for i in range(0, len(key_string), 2):
-        if row < 4 and col < 4:
-            if len(key_string[i:i + 2]) == 1:
-                arr[row][col] = key_string[i:i + 2] + "0"
-            else:
-                arr[row][col] = key_string[i:i + 2]
-            col = col + 1
-            if col > 3:
-                row = row + 1
-                col = 0
-    return arr
-
-
-rnd_const = [["01", "00", "00", "00"], ["02", "00", "00", "00"], ["04", "00", "00", "00"], ["08", "00", "00", "00"],
-             ["10", "00", "00", "00"], ["20", "00", "00", "00"], ["40", "00", "00", "00"], ["80", "00", "00", "00"],
-             ["1b", "00", "00", "00"], ["36", "00", "00", "00"]]
-
-
-def aes_encryption(plain_text, aes_key):
-    add_round_key = add_round(plain_text, aes_key)
-    sub_byte = substitute_byte(add_round_key)
-    shift_rows = shift_row(sub_byte)
-    mix_column = mix_col(row_to_col(shift_rows))
-    add_round_key = add_round(mix_column, key_expansion(aes_key, rnd_const[0]))
-    aes_key = key_expansion(aes_key, rnd_const[0])
-
-    for i in range(1, 9):
-        tmp_key = key_expansion(aes_key, rnd_const[i])
-        aes_key = tmp_key
-        sub_byte = substitute_byte(add_round_key)
-        shift_rows = shift_row(sub_byte)
-        mix_column = mix_col(row_to_col(shift_rows))
-        add_round_key = add_round(mix_column, aes_key)
-
-    sub_byte = substitute_byte(add_round_key)
-    shift_rows = row_to_col(shift_row(sub_byte))
-    tmp_key = key_expansion(aes_key, rnd_const[9])
-    aes_key = tmp_key
-    add_round_key = add_round(shift_rows, aes_key)
-
-    cipher = ""
-    for row in range(0, len(add_round_key)):
-        for col in range(0, 4):
-            cipher = cipher + add_round_key[col][row]
-    return cipher
-
-
-plaintext = input("Enter text: ")
-valid_pt_block = valid_block_size(plaintext)
-plaintext_matrix = key_and_text_to_matrix(plaintext)
-print("Plaintext Block:", plaintext_matrix)
-print("---------------------------------------------------------------------------------------------------------------")
-
-key = input("Enter key: ")
-valid_k_block = valid_block_size(key)
-key_matrix = key_and_text_to_matrix(key)
-print("Key Block:", key_matrix)
-print("---------------------------------------------------------------------------------------------------------------")
-
-cipher_text = aes_encryption(plaintext_matrix, key_matrix)
-print("Encrypted Text: ", cipher_text)
-print("---------------------------------------------------------------------------------------------------------------")
+# take a master key as input and return a list of round keys.
+def generateRoundKeys(key):
+    roundKeys = matrixFromText(key)  # Convert the master key to a matrix representation
+
+    for i in range(4, 4 * 11):
+        roundKeys.append([])  # Append an empty list for each round key
+
+        if i % 4 == 0:
+            byte = roundKeys[i - 4][0] \
+                   ^ SubBytesTuple[roundKeys[i - 1][1]] \
+                   ^ RCON[i // 4]  # XOR the bytes from the previous round key
+            roundKeys[i].append(byte)  # Add the XORed byte to the current round key
+
+            for j in range(1, 4):
+                byte = roundKeys[i - 4][j] \
+                       ^ SubBytesTuple[roundKeys[i - 1][(j + 1) % 4]]  # XOR the bytes from the previous round key
+                roundKeys[i].append(byte)  # Add the XORed byte to the current round key
+        else:
+            for j in range(4):
+                byte = roundKeys[i - 4][j] \
+                       ^ roundKeys[i - 1][j]  # XOR the bytes from the previous round key
+                roundKeys[i].append(byte)  # Add the XORed byte to the current round key
+
+    return roundKeys
+
+# This function replaces each byte in the state matrix with its corresponding value from the SubBytesTuple.
+def fillWithSubBytes(s):
+    for i in range(4):
+        for j in range(4):
+            s[i][j] = SubBytesTuple[s[i][j]]
+
+# This function replaces each byte in the state matrix with its corresponding value from the InversedSubBytesTyple.
+def fillWithInverseSubBytes(s):
+    for i in range(4):
+        for j in range(4):
+            s[i][j] = InversedSubBytesTyple[s[i][j]]
+
+# This function shifts the rows of the state matrix.
+def shiftRows(s):
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[1][1], s[2][1], s[3][1], s[0][1]
+    s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[3][3], s[0][3], s[1][3], s[2][3]
+
+
+def inverseShiftRows(s):
+    s[0][1], s[1][1], s[2][1], s[3][1] = s[3][1], s[0][1], s[1][1], s[2][1]
+    s[0][2], s[1][2], s[2][2], s[3][2] = s[2][2], s[3][2], s[0][2], s[1][2]
+    s[0][3], s[1][3], s[2][3], s[3][3] = s[1][3], s[2][3], s[3][3], s[0][3]
+
+def mixOneCol(a):
+    t = a[0] ^ a[1] ^ a[2] ^ a[3]
+    u = a[0]
+    a[0] ^= t ^ xtime(a[0] ^ a[1])
+    a[1] ^= t ^ xtime(a[1] ^ a[2])
+    a[2] ^= t ^ xtime(a[2] ^ a[3])
+    a[3] ^= t ^ xtime(a[3] ^ u)
+
+def mixCols(s):
+    for i in range(4):
+        mixOneCol(s[i])
+
+def inverseMixCols(s):
+    for i in range(4):
+        u = xtime(xtime(s[i][0] ^ s[i][2]))
+        v = xtime(xtime(s[i][1] ^ s[i][3]))
+        s[i][0] ^= u
+        s[i][1] ^= v
+        s[i][2] ^= u
+        s[i][3] ^= v
+
+    mixCols(s)
+
+def addRoundKey(s, k):
+    for i in range(4):
+        for j in range(4):
+            s[i][j] ^= k[i][j]
+
+def encrypt(plaintext, roundKeys):
+    state = matrixFromText(plaintext)
+
+    addRoundKey(state, roundKeys[:4])
+
+    for i in range(1, 10):
+        key_matrix = roundKeys[4 * i: 4 * (i + 1)]
+        fillWithSubBytes(state)
+        shiftRows(state)
+        mixCols(state)
+        addRoundKey(state, key_matrix)
+
+    fillWithSubBytes(state)
+    shiftRows(state)
+    addRoundKey(state, roundKeys[40:])
+
+    return textFromMatrix(state)
+
+def decrypt(ciphertext, roundKeys):
+    state = matrixFromText(ciphertext)
+
+    addRoundKey(state, roundKeys[40:])
+    inverseShiftRows(state)
+    fillWithInverseSubBytes(state)
+
+    for i in range(9, 0, -1):
+        key_matrix = roundKeys[4 * i: 4 * (i + 1)]
+        addRoundKey(state, key_matrix)
+        inverseMixCols(state)
+        inverseShiftRows(state)
+        fillWithInverseSubBytes(state)
+
+    addRoundKey(state, roundKeys[:4])
+
+    return textFromMatrix(state)
+
+SubBytesTuple = (
+    0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
+    0xCA, 0x82, 0xC9, 0x7D, 0xFA, 0x59, 0x47, 0xF0, 0xAD, 0xD4, 0xA2, 0xAF, 0x9C, 0xA4, 0x72, 0xC0,
+    0xB7, 0xFD, 0x93, 0x26, 0x36, 0x3F, 0xF7, 0xCC, 0x34, 0xA5, 0xE5, 0xF1, 0x71, 0xD8, 0x31, 0x15,
+    0x04, 0xC7, 0x23, 0xC3, 0x18, 0x96, 0x05, 0x9A, 0x07, 0x12, 0x80, 0xE2, 0xEB, 0x27, 0xB2, 0x75,
+    0x09, 0x83, 0x2C, 0x1A, 0x1B, 0x6E, 0x5A, 0xA0, 0x52, 0x3B, 0xD6, 0xB3, 0x29, 0xE3, 0x2F, 0x84,
+    0x53, 0xD1, 0x00, 0xED, 0x20, 0xFC, 0xB1, 0x5B, 0x6A, 0xCB, 0xBE, 0x39, 0x4A, 0x4C, 0x58, 0xCF,
+    0xD0, 0xEF, 0xAA, 0xFB, 0x43, 0x4D, 0x33, 0x85, 0x45, 0xF9, 0x02, 0x7F, 0x50, 0x3C, 0x9F, 0xA8,
+    0x51, 0xA3, 0x40, 0x8F, 0x92, 0x9D, 0x38, 0xF5, 0xBC, 0xB6, 0xDA, 0x21, 0x10, 0xFF, 0xF3, 0xD2,
+    0xCD, 0x0C, 0x13, 0xEC, 0x5F, 0x97, 0x44, 0x17, 0xC4, 0xA7, 0x7E, 0x3D, 0x64, 0x5D, 0x19, 0x73,
+    0x60, 0x81, 0x4F, 0xDC, 0x22, 0x2A, 0x90, 0x88, 0x46, 0xEE, 0xB8, 0x14, 0xDE, 0x5E, 0x0B, 0xDB,
+    0xE0, 0x32, 0x3A, 0x0A, 0x49, 0x06, 0x24, 0x5C, 0xC2, 0xD3, 0xAC, 0x62, 0x91, 0x95, 0xE4, 0x79,
+    0xE7, 0xC8, 0x37, 0x6D, 0x8D, 0xD5, 0x4E, 0xA9, 0x6C, 0x56, 0xF4, 0xEA, 0x65, 0x7A, 0xAE, 0x08,
+    0xBA, 0x78, 0x25, 0x2E, 0x1C, 0xA6, 0xB4, 0xC6, 0xE8, 0xDD, 0x74, 0x1F, 0x4B, 0xBD, 0x8B, 0x8A,
+    0x70, 0x3E, 0xB5, 0x66, 0x48, 0x03, 0xF6, 0x0E, 0x61, 0x35, 0x57, 0xB9, 0x86, 0xC1, 0x1D, 0x9E,
+    0xE1, 0xF8, 0x98, 0x11, 0x69, 0xD9, 0x8E, 0x94, 0x9B, 0x1E, 0x87, 0xE9, 0xCE, 0x55, 0x28, 0xDF,
+    0x8C, 0xA1, 0x89, 0x0D, 0xBF, 0xE6, 0x42, 0x68, 0x41, 0x99, 0x2D, 0x0F, 0xB0, 0x54, 0xBB, 0x16,
+)
+
+InversedSubBytesTyple = (
+    0x52, 0x09, 0x6A, 0xD5, 0x30, 0x36, 0xA5, 0x38, 0xBF, 0x40, 0xA3, 0x9E, 0x81, 0xF3, 0xD7, 0xFB,
+    0x7C, 0xE3, 0x39, 0x82, 0x9B, 0x2F, 0xFF, 0x87, 0x34, 0x8E, 0x43, 0x44, 0xC4, 0xDE, 0xE9, 0xCB,
+    0x54, 0x7B, 0x94, 0x32, 0xA6, 0xC2, 0x23, 0x3D, 0xEE, 0x4C, 0x95, 0x0B, 0x42, 0xFA, 0xC3, 0x4E,
+    0x08, 0x2E, 0xA1, 0x66, 0x28, 0xD9, 0x24, 0xB2, 0x76, 0x5B, 0xA2, 0x49, 0x6D, 0x8B, 0xD1, 0x25,
+    0x72, 0xF8, 0xF6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xD4, 0xA4, 0x5C, 0xCC, 0x5D, 0x65, 0xB6, 0x92,
+    0x6C, 0x70, 0x48, 0x50, 0xFD, 0xED, 0xB9, 0xDA, 0x5E, 0x15, 0x46, 0x57, 0xA7, 0x8D, 0x9D, 0x84,
+    0x90, 0xD8, 0xAB, 0x00, 0x8C, 0xBC, 0xD3, 0x0A, 0xF7, 0xE4, 0x58, 0x05, 0xB8, 0xB3, 0x45, 0x06,
+    0xD0, 0x2C, 0x1E, 0x8F, 0xCA, 0x3F, 0x0F, 0x02, 0xC1, 0xAF, 0xBD, 0x03, 0x01, 0x13, 0x8A, 0x6B,
+    0x3A, 0x91, 0x11, 0x41, 0x4F, 0x67, 0xDC, 0xEA, 0x97, 0xF2, 0xCF, 0xCE, 0xF0, 0xB4, 0xE6, 0x73,
+    0x96, 0xAC, 0x74, 0x22, 0xE7, 0xAD, 0x35, 0x85, 0xE2, 0xF9, 0x37, 0xE8, 0x1C, 0x75, 0xDF, 0x6E,
+    0x47, 0xF1, 0x1A, 0x71, 0x1D, 0x29, 0xC5, 0x89, 0x6F, 0xB7, 0x62, 0x0E, 0xAA, 0x18, 0xBE, 0x1B,
+    0xFC, 0x56, 0x3E, 0x4B, 0xC6, 0xD2, 0x79, 0x20, 0x9A, 0xDB, 0xC0, 0xFE, 0x78, 0xCD, 0x5A, 0xF4,
+    0x1F, 0xDD, 0xA8, 0x33, 0x88, 0x07, 0xC7, 0x31, 0xB1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xEC, 0x5F,
+    0x60, 0x51, 0x7F, 0xA9, 0x19, 0xB5, 0x4A, 0x0D, 0x2D, 0xE5, 0x7A, 0x9F, 0x93, 0xC9, 0x9C, 0xEF,
+    0xA0, 0xE0, 0x3B, 0x4D, 0xAE, 0x2A, 0xF5, 0xB0, 0xC8, 0xEB, 0xBB, 0x3C, 0x83, 0x53, 0x99, 0x61,
+    0x17, 0x2B, 0x04, 0x7E, 0xBA, 0x77, 0xD6, 0x26, 0xE1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0C, 0x7D,
+)
+
+
+xtime = lambda a: (((a << 1) ^ 0x1B) & 0xFF) if (a & 0x80) else (a << 1)
+
+RCON = (
+    0x00, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40,
+    0x80, 0x1B, 0x36, 0x6C, 0xD8, 0xAB, 0x4D, 0x9A,
+    0x2F, 0x5E, 0xBC, 0x63, 0xC6, 0x97, 0x35, 0x6A,
+    0xD4, 0xB3, 0x7D, 0xFA, 0xEF, 0xC5, 0x91, 0x39,
+)
+
+
+# This function converts a matrix to a hexadecimal string representation.
+def matrixFromText(text):
+    matrix = []
+    for i in range(16):
+        byte = (text >> (8 * (15 - i))) & 0xFF
+        if i % 4 == 0:
+            matrix.append([byte])
+        else:
+            matrix[i // 4].append(byte)
+    return matrix
+
+# This function converts a hexadecimal string to a matrix representation.
+def textFromMatrix(matrix):
+    text = 0
+    for i in range(4):
+        for j in range(4):
+            text |= (matrix[i][j] << (120 - 8 * (4 * i + j)))
+    return text
+
+key = 0x2b7e151628aed2a6abf7158809cf4f3c
+roundKeys = generateRoundKeys(key)
+
+data = 13
+decrypted = decrypt(data, roundKeys)
+encrypted = encrypt(decrypted, roundKeys)
+
+if(data == encrypted):
+    print("Correct")
+else:
+    print("Not correct")
+    
+print("Data: ", data)
+print("Decrypted data: ", decrypted)
--- Grade3(2022-2023)/Cryptology/SignatureAlgorithmDSA.py
+++ Grade3(2022-2023)/Cryptology/SignatureAlgorithmDSA.py
@@ -3,98 +3,107 @@ from hashlib import sha1
 from gmpy2 import xmpz, to_binary, invert, powmod, is_prime
 
 
-def generate_p_q(L, N):
-    n = (L - 1) // g
-    b = (L - 1) % g
+def generatePQ(bitLength, securityParameter):
+    # Generate large prime numbers p and q
+    primeBitLength = securityParameter  # primeBitLength >= 160
+    primeCount = (bitLength - 1) // primeBitLength
+    bitCount = (bitLength - 1) % primeBitLength
     while True:
+        # Generate q
         while True:
-            s = xmpz(randrange(1, 2 ** (g)))
-            a = sha1(to_binary(s)).hexdigest()
-            zz = xmpz((s + 1) % (2 ** g))
-            z = sha1(to_binary(zz)).hexdigest()
+            seed = xmpz(randrange(1, 2 ** (primeBitLength)))
+            a = sha1(to_binary(seed)).hexdigest()
+            seedPlusOne = xmpz((seed + 1) % (2 ** primeBitLength))
+            z = sha1(to_binary(seedPlusOne)).hexdigest()
             U = int(a, 16) ^ int(z, 16)
-            mask = 2 ** (N - 1) + 1
+            mask = 2 ** (securityParameter - 1) + 1
             q = U | mask
             if is_prime(q, 20):
                 break
-        i = 0 
-        j = 2 
-        while i < 4096:
+        # Generate p
+        counter = 0  # Counter
+        offset = 2  # Offset
+        while counter < 4096:
             V = []
-            for k in range(n + 1):
-                arg = xmpz((s + j + k) % (2 ** g))
+            for primeIndex in range(primeCount + 1):
+                arg = xmpz((seed + offset + primeIndex) % (2 ** primeBitLength))
                 zzv = sha1(to_binary(arg)).hexdigest()
                 V.append(int(zzv, 16))
-            W = 0
-            for qq in range(0, n):
-                W += V[qq] * 2 ** (160 * qq)
-            W += (V[n] % 2 ** b) * 2 ** (160 * n)
-            X = W + 2 ** (L - 1)
+            W_acc = 0
+            for primeIndex in range(0, primeCount):
+                W_acc += V[primeIndex] * 2 ** (160 * primeIndex)
+            W_acc += (V[primeCount] % 2 ** bitCount) * 2 ** (160 * primeCount)
+            X = W_acc + 2 ** (bitLength - 1)
             c = X % (2 * q)
             p = X - c + 1  # p = X - (c - 1)
-            if p >= 2 ** (L - 1):
+            if p >= 2 ** (bitLength - 1):
                 if is_prime(p, 10):
                     return p, q
-            i += 1
-            j += n + 1
+            counter += 1
+            offset += primeCount + 1
 
 
-def generate_g(p, q):
+def generateG(p, q):
+    # Generate a generator g
     while True:
         h = randrange(2, p - 1)
-        exp = xmpz((p - 1) // q)
-        g = powmod(h, exp, p)
+        exponent = xmpz((p - 1) // q)
+        g = powmod(h, exponent, p)
         if g > 1:
             break
     return g
 
 
-def generate_keys(g, p, q):
-    x = randrange(2, q)  # x < q
-    y = powmod(g, x, p)
-    return x, y
+def generateKeys(g, p, q):
+    # Generate public and private keys
+    privateKey = randrange(2, q)  # privateKey < q
+    publicKey = powmod(g, privateKey, p)
+    return privateKey, publicKey
 
 
-def generate_params(L, N):
-    p, q = generate_p_q(L, N)
-    g = generate_g(p, q)
+def generateParameters(bitLength, securityParameter):
+    # Generate prime number parameters p, q, and generator g
+    p, q = generatePQ(bitLength, securityParameter)
+    g = generateG(p, q)
     return p, q, g
 
 
-def sign(M, p, q, g, x):
-    if not validate_params(p, q, g):
-        raise Exception("Invalid params")
+def sign(message, p, q, g, privateKey):
+    # Generate a signature for the message
+    if not validateParameters(p, q, g):
+        raise Exception("Invalid parameters")
     while True:
         k = randrange(2, q)  # k < q
         r = powmod(g, k, p) % q
-        m = int(sha1(M).hexdigest(), 16)
+        messageHash = int(sha1(message).hexdigest(), 16)
         try:
-            s = (invert(k, q) * (m + x * r)) % q
+            s = (invert(k, q) * (messageHash + privateKey * r)) % q
             return r, s
         except ZeroDivisionError:
             pass
 
 
-def verify(M, r, s, p, q, g, y):
-    if not validate_params(p, q, g):
-        raise Exception("Invalid params")
-    if not validate_sign(r, s, q):
+def verify(message, r, s, p, q, g, publicKey):
+    # Verify the signature of a message
+    if not validateParameters(p, q, g):
+        raise Exception("Invalid parameters")
+    if not validateSignature(r, s, q):
         return False
     try:
         w = invert(s, q)
     except ZeroDivisionError:
         return False
-    m = int(sha1(M).hexdigest(), 16)
-    u1 = (m * w) % q
+    messageHash = int(sha1(message).hexdigest(), 16)
+    u1 = (messageHash * w) % q
     u2 = (r * w) % q
-    # v = ((g ** u1 * y ** u2) % p) % q
-    v = (powmod(g, u1, p) * powmod(y, u2, p)) % p % q
+    v = (powmod(g, u1, p) * powmod(publicKey, u2, p)) % p % q
     if v == r:
         return True
     return False
 
 
-def validate_params(p, q, g):
+def validateParameters(p, q, g):
+    # Validate the parameters p, q, g
     if is_prime(p) and is_prime(q):
         return True
     if powmod(g, q, p) == 1 and g > 1 and (p - 1) % q:
@@ -102,22 +111,24 @@ def validate_params(p, q, g):
     return False
 
 
-def validate_sign(r, s, q):
-    if r < 0 and r > q:
+def validateSignature(r, s, q):
+    # Validate the signature values r, s
+    if r < 0 or r > q:
         return False
-    if s < 0 and s > q:
+    if s < 0 or s > q:
         return False
     return True
 
 
-N = 160
-L = 1024
-p, q, g = generate_params(L, N)
-x, y = generate_keys(g, p, q)
+securityParameter = 160
+bitLength = 1024
+p, q, g = generateParameters(bitLength, securityParameter)
+privateKey, publicKey = generateKeys(g, p, q)
 
-text = "Algorithm test"
-M = str.encode(text, "ascii")
-r, s = sign(M, p, q, g, x)
-if verify(M, r, s, p, q, g, y):
-    print('All ok')
-print(M, r, s, p, q, g, y, x, sep='\n')
+message = "Some message"
+messageBytes = str.encode(message, "ascii")
+r, s = sign(messageBytes, p, q, g, privateKey)
+print("public key: ", publicKey)
+print("private key: ", privateKey)
+if verify(messageBytes, r, s, p, q, g, publicKey):
+    print('Correct')
